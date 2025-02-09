47a20c4d948c811eb82c3eab20233c1171487c06
Kamal Banjade <kamalbanjade67@gmail.com>
2025-02-09 21:34:53 +0545

    initialized navbar

--- src/MyRoutes.jsx
+++ src/MyRoutes.jsx
@@ -3,10 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Products from './components/Products/products';
+import Navbar from './Navbar/Navbar';
+
 
 const MyRoutes = () => {
   return (
     <>
+    <Navbar />
       <Routes>
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register />} />
