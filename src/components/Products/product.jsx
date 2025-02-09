169959499fe4003ed5b8e0ce6a2bab377270b208
BimochanBhattarai <itsbimochan@gmail.com>
2025-02-09 15:05:16 +0545

    Product

---
+++ src/components/Products/product.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const product = () => {
+  return (
+    <div>
+        <div className='h-48 w-48'>
+            <img src="" alt="Product Picture" />
+        </div>
+        <div>
+            <div>
+                <h3>Product Name</h3>
+                Grade : A
+            </div>
+            <h3>Product Price</h3>
+        </div>
+    </div>
+  )
+}
+
+export default product
