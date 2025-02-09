5f2d3c74d44b99932941fe552729220adc1f0312
BimochanBhattarai <itsbimochan@gmail.com>
2025-02-09 20:03:59 +0545

    Add bid page

--- src/MyRoutes.jsx
+++ src/MyRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Products from './components/Products/products';
+import Bid from './components/Bid/Bid';
 
 const MyRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const MyRoutes = () => {
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/bid" element={<Bid />} />
         {/* <Route path="/buy" element={<Buy />} />
         <Route path="/sell" element={<Sell />} /> */}
       </Routes>
--- src/components/Bid/Bid.jsx
+++ src/components/Bid/Bid.jsx
@@ -1,41 +1,47 @@
 import { Button, TextField } from '@mui/material';
 import React from 'react';
+import Products from '../Products/products';
 
 const Bid = () => {
   return (
-    <div>
-      <div>
+    <>
+    <div className='flex p-10 mx-auto w-4/5 mb-10'>
+      <div >
+        <div className='rounded-xl overflow-hidden mr-10 '>
         <img src="./image.png" alt="Garbage Image" />
-      </div>
-      <div>
-        <div>
-          <h1>Plastic</h1>
-          <h3>Used bottles and plastic</h3>
         </div>
+      </div>
+      <div className='w-2/3'>
         <div>
-            <span>
+          <h1 className='text-3xl font-bold mb-2'>Plastic</h1>
+          <h3 className='text-xl mb-3'>Used bottles and plastic</h3>
+        </div>
+        <div className='flex justify-between mb-3'>
+            <span className='font-bold text-green-500'>
                 Highest Bid : Rs. 200
             </span>
-            <span>
+            <span className='font-bold text-red-500'>
                 Lowest Bid : Rs. 100
             </span>
-            <span>
+            <span className='font-bold text-blue-500'>
                 Last Bid : Rs. 150
             </span>
         </div>
-        <div>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <div className='flex gap-10'>
+        <TextField id="outlined-basic" label="Enter your bid." variant="outlined" className='w-full'/>
         <Button
             color="success"
             size="large"
             variant="contained"
-            className="w-full"
+            className="w-1/3"
           >
             Bid
           </Button>
         </div>
       </div>
     </div>
+    <Products />
+    </>
   );
 };
 
