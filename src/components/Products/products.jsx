315dc45c4d19c2d262167b5c43364f4d85bc2dee
Ankit Diyal <125892237+anky17@users.noreply.github.com>
2025-02-09 22:35:43 +0545

    responsiveness added

--- src/components/Products/products.jsx
+++ src/components/Products/products.jsx
@@ -1,17 +1,38 @@
+// import React from 'react';
+// import Product from './product';
+
+// const Products = () => {
+//   return (
+//     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+//       <Product />
+//       <Product />
+//       <Product />
+//       <Product />
+//       <Product />
+//       <Product />
+//       <Product />
+//       <Product />
+//     </div>
+//   );
+// };
+
+// export default Products;
 import React from 'react';
 import Product from './product';
 
 const Products = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+    <div className="container mx-auto px-8 py-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
     </div>
   );
 };
