f57196525b39fad9bd81f5d259c64e94ced25eef
BimochanBhattarai <itsbimochan@gmail.com>
2025-02-09 19:08:03 +0545

    add Card Feature

--- src/components/Products/product.jsx
+++ src/components/Products/product.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import { Button, CardActions, CardMedia, Typography } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import React from 'react';
 
 const Product = () => {
   return (
-    <div>
-        <div className='h-48 w-48'>
-            <img src="" alt="Product Picture" />
-        </div>
-        <div>
-            <div>
-                <h3>Product Name</h3>
-                Grade : A
-            </div>
-            <h3>Product Price</h3>
-        </div>
+    <div className='p-5'>
+      <Card sx={{ maxWidth: 300 }}>
+        <CardMedia sx={{ height: 200 }} image="./public/image.png" />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            style={{ marginBottom: '-1px' }}
+          >
+            Plastic
+          </Typography>
+          <Typography
+            style={{ color: 'black', fontSize: '16px', marginBottom: '5px' }}
+            variant="body2"
+            sx={{ color: 'text.secondary' }}
+          >
+            Used bottles and plastic
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            <span className="font-bold text-green-500">
+              Current Bid : Rs. 200
+            </span>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            color="success"
+            size="large"
+            variant="contained"
+            className="w-full"
+          >
+            Bid
+          </Button>
+        </CardActions>
+      </Card>
     </div>
-  )
-}
-
-export default Product
+  );
+};
+export default Product;
--- src/components/Products/products.jsx
+++ src/components/Products/products.jsx
@@ -3,7 +3,16 @@ import Product from './product';
 
 const Products = () => {
   return (
-    <div>
+    <div 
+    className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'
+    >
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
         <Product />
     </div>
   )
